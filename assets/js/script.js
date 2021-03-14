ea00d6c32450d58fcfb94fd95ab9a5be01ccf93f
Christina Pagan <enterprise@christinas-mbp.home>
2021-03-13 23:36:10 -0500

    added prompts and alert for generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,46 @@ var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 var numChar = '0123456789';
 var specialChar = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
 
-//Create array to store password characters in 
+//Create array to store password characters in, strings will be added as user picks "ok=true"
 charArray = [upperCase, lowerCase, numChar, specialChar];
+//creating global variable for password length
+var userLength;
+
+function checkLength(userLength)
+{
+  userLength = prompt('Please choose a password length from 8 to 128.');
+
+  if(userLength < 8 || userLength > 128 || userLength === null)
+  {
+    alert('Please choose a number from 8 to 128.');
+    generatePassword();
+  }
+  else
+  {
+    userChoices();
+    //continuing program here
+  }
+}
+
+function userChoices()
+{
+//prompts for user choices ok = true cancel = false
+var userUpper = confirm("Would you like to have uppercase letters?");
+var userLower = confirm("Would you like to have lowercase letters?");
+var userNum = confirm("Would you like to have numbers?");
+var userSpec = confirm("Would you like to have special characters?");
+//storing user choices in an array
+userChoice = [userUpper, userLower, userNum, userSpec];
+//creating an empty array to add user passward character options below 
+emptyArray = [];
+
+}
+
+function generatePassword()
+{
+  checkLength();
+}
+
 
 
 // Get references to the #generate element
