d16382f344a4abfa8d4a0dae08d61121fdecaad4
Christina Pagan <enterprise@christinas-mbp.home>
2021-03-14 00:23:48 -0500

    made final changes, fixed errors with password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,39 +8,30 @@ var specialChar = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
 
 //Create array to store password characters in, strings will be added as user picks "ok=true"
 charArray = [upperCase, lowerCase, numChar, specialChar];
-//creating global variable for password length
-var userLength;
-//empty string to hold user password character choices 
-var sumChar = '';
 
-function checkLength()
+
+function generatePassword()
 {
-  userLength = prompt('Please choose a password length from 8 to 128.');
+
+  var userLength = prompt('Please choose a password length from 8 to 128.');
+  
   userLength = parseInt(userLength);
 
   if(userLength < 8 || userLength > 128 || userLength === null)
   {
-    alert('Please choose a number from 8 to 128.');
-    generatePassword();
+   alert('Please type 8 to 128');
+   return 'Please click the button again :).';
   }
-  else
-  {
-    userChoices();
-    //continuing program here
-  }
-  return userLength;
-}
+else {
 
-function userChoices()
-{
-//prompts for user choices ok = true cancel = false
+//prompts for user choices ok = true cancel = false?
 var userUpper = confirm("Would you like to have uppercase letters?");
 var userLower = confirm("Would you like to have lowercase letters?");
 var userNum = confirm("Would you like to have numbers?");
 var userSpec = confirm("Would you like to have special characters?");
 //storing user choices in an array
 userChoice = [userUpper, userLower, userNum, userSpec];
-//creating an empty array to add user password character options below 
+//creating an empty array to add user password character options 
 emptyArray = [];
 
 for( i = 0; i < userChoice.length; i++)
@@ -50,26 +41,22 @@ for( i = 0; i < userChoice.length; i++)
   //above will add the password character strings to an empty array
 }
 //this for loop below will add all of the wanted characters together in a string variable
+var sumChar = '';
 for(i = 0; i < emptyArray.length; i++)
 {
   sumChar = sumChar + emptyArray[i];
 }
-return sumChar;
-}
 
-function generatePassword()
-{
-  checkLength();
-  //now to mix the characters into a randomly generated password based on user choices 
-  var genPassword = '';
+//this loop will take the newly formed string sumChar and randomize the characters for the user
+  var genPassword= '';
   for(i = 0; i < userLength; i++)
   {
-    genPassword = genPassword + sumChar.charAt(Math.floor(Math.random() * sumChar.length));
+  genPassword = genPassword + sumChar.charAt(Math.floor(Math.random() * sumChar.length));
   }
   return genPassword;
+
 }
-
-
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
